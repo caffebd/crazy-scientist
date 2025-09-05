527354286d7e4e8886d50cbfbfbb39b7ab4b9960
sayem_CAFFE <sayem@caffebd.org>
2025-09-05 16:21:10 +0600

    statubar_start

--- script/enemy.gd
+++ script/enemy.gd
@@ -2,4 +2,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bullet"):
-		queue_free()
+		%ProgressBar.value -= 5
+		if %ProgressBar.value == 0:
+			queue_free()
