588b90f02328a750aa5858ff19bac68f678afc19
sayem_CAFFE <sayem@caffebd.org>
2025-09-10 17:10:49 +0600

    level

--- script/ladder.gd
+++ script/ladder.gd
@@ -1,14 +1,10 @@
 extends Area2D
 
+@export var flip_h : Sprite2D
+@export var collition : CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
